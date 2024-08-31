eb9a8a088d0bcfe76fb607b1a700478d2d51e385
kenousha <154089171+kenousha@users.noreply.github.com>
2024-08-31 12:09:26 -0400

    app.js landingpage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
-import LandingPage from './components/LandingPage/LandingPage';
+import LandingPage from './Components/LandingPage/LandingPage';
 function App() {
 
   return (
